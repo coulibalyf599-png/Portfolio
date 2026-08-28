55f97c395034d4be4faa2ed39631bb52ad38313d
coulibalyf599-png <coulibalyf599@gmail.com>
2026-08-28 10:27:24 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Front-End Developpeur", "Back-End Developpeur", "Designer" ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
